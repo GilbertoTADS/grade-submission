38861d80387d61c6f9daa51a61fc165c2c2e6565
GilbertoTADS <gilbertotads@gmail.com>
2023-03-05 19:32:38 -0300

    add conditionals resources

---
+++ src/main/java/com/dev/gradesubmission/ConditionalController.java
@@ -0,0 +1,15 @@
+package com.dev.gradesubmission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConditionalController {
+
+    @GetMapping(value="/")
+    public String getMethodName(Model model){
+        model.addAttribute("sales",3);
+        return "conditionals";
+    }
+}
