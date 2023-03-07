99a6e1d77ed4f2153f8180f68b18f7c0905a9a26
GilbertoTADS <gilbertotads@gmail.com>
2023-03-07 06:35:22 -0300

    Lesson: utility methdos from thymeleaf

---
+++ src/main/java/com/dev/gradesubmission/WorkbookController.java
@@ -0,0 +1,15 @@
+package com.dev.gradesubmission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WorkbookController {
+    
+    @GetMapping(value="/utility-methods")
+    public String getNameMethod(Model model){
+        model.addAttribute("menu", "We sell chocolate rice cakes buble tea");
+        return "view";
+    }
+}
