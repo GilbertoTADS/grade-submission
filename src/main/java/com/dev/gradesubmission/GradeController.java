abee4af0643d4ce7bdb1193e09c3127045dfcb9f
GilbertoTADS <gilbertotads@gmail.com>
2023-05-26 05:54:47 -0300

    button update grade creation and fix method to receive request

--- src/main/java/com/dev/gradesubmission/GradeController.java
+++ src/main/java/com/dev/gradesubmission/GradeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
@@ -14,8 +15,13 @@ public class GradeController {
     private List<Grade> grades = new ArrayList<>();
     
     @GetMapping(value="/")
-    public String gradeForm(Model model){
-        Grade grade = new Grade("","","");
+    public String gradeForm(Model model, @RequestParam(required = false) String name){
+        Integer indexGrade = getGradeIndex(name);
+        boolean gradeNotExist = indexGrade == -1000;
+        
+        Grade gradeEmpty = new Grade("","","");
+        Grade grade = gradeNotExist ? gradeEmpty : grades.get(indexGrade);
+        
         model.addAttribute("grade", grade);
         return "form";
     }
@@ -27,8 +33,21 @@ public class GradeController {
     }
     @PostMapping("/handleSubmit")
     public String submitGrade(Grade grade){
-        grades.add(grade);
+        Integer indexGrade = getGradeIndex(grade.getName());
+        boolean gradeNotExist = indexGrade == -1000;
+        
+        if(gradeNotExist){
+            grades.add(grade);
+         }else{
+            grades.set(indexGrade, grade);
+         }
         return "redirect:/grades";
+    }
+    public Integer getGradeIndex(String name){
+        for(int i = 0; i < grades.size();i++){
+            if(grades.get(i).getName().equals(name)) return i;
+        }
+        return -1000;
     }
     
 }
