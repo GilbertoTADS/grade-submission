fdcf2ecc9cf4d09f5e7d06497d8ba37a76c06b0d
GilbertoTADS <gilbertotads@gmail.com>
2023-06-09 15:38:44 -0300

    initial commit

---
+++ src/main/java/br/com/dev/globalstore/GlobalStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.globalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dev/globalstore/GlobalStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.globalstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
